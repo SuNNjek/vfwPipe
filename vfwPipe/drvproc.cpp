eef413d66758ff1f882024448b9da3ba2dd1a8c2
Robin Heinemeier <sunnerlp@outlook.com>
2016-06-26 14:07:20 +0200

    Added license txt and headers

--- vfwPipe/drvproc.cpp
+++ vfwPipe/drvproc.cpp
@@ -1,4 +1,23 @@
-// dllmain.cpp : Definiert den Einstiegspunkt für die DLL-Anwendung.
+/**
+* vfwPipe, vfw codec which pipes its output to a console application like ffmpeg
+*
+* Copyright (C) 2016 Robin Heinemeier
+*
+* This file is part of vfwPipe.
+*
+* vfwPipe is free software: you can redistribute it and/or modify it under the terms of the
+* GNU General Public License as published by the Free Software Foundation, either version 2 of the
+* License, or (at your option) any later version.
+*
+* vfwPipe is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*
+* See the GNU General Public License for more details. You should have received a copy of the GNU
+* General Public License along with vfwPipe. If not, see <http://www.gnu.org/licenses/>.
+*
+* Authors: Robin Heinemeier
+*/
+
 #include <Windows.h>
 #include <Vfw.h>
 #include <sstream>
--- vfwPipe/pipeHandler.h
+++ vfwPipe/pipeHandler.h
@@ -1,3 +1,23 @@
+/**
+* vfwPipe, vfw codec which pipes its output to a console application like ffmpeg
+*
+* Copyright (C) 2016 Robin Heinemeier
+*
+* This file is part of vfwPipe.
+*
+* vfwPipe is free software: you can redistribute it and/or modify it under the terms of the
+* GNU General Public License as published by the Free Software Foundation, either version 2 of the
+* License, or (at your option) any later version.
+*
+* vfwPipe is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*
+* See the GNU General Public License for more details. You should have received a copy of the GNU
+* General Public License along with vfwPipe. If not, see <http://www.gnu.org/licenses/>.
+*
+* Authors: Robin Heinemeier
+*/
+
 #ifndef __PIPE_HANDLER_H_
 #define __PIPE_HANDLER_H_
 
